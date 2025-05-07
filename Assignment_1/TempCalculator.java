baaa3a06ce46356d2528cff4bf0c6049fc796f5e
Noah-Hickey <Noah.Hickey@keyin.com>
2025-05-07 16:41:40 -0230

    Began project. Added in print statements.

---
+++ Assignment_1/TempCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TempCalculator {
+
+    public static void main(String[] args) {
+        
+    System.out.println("Welcome to the Temperature Converter!");
+    System.out.println();
+    System.out.println("Please enter 5 temperatures in Celsius to get the average.");
+
+    System.out.println();
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the first temperature in Celsius: ");
+    
+
+    System.out.println("Enter the second temperature in Celsius: ");
+
+    System.out.println("Enter the third temperature in Celsius: ");
+
+    System.out.println();
+    scanner.close();
+
+    }
+}
