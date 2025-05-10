68a0d32bd0899f311118d303fa970950c611bf34
Noah-Hickey <Noah.Hickey@keyin.com>
2025-05-10 16:22:32 -0230

    Completed testing on the program. Added in a loop that will increment a value based on if the temperature is above the average. Also made the print statements neater.

--- Assignment_1/TempCalculator.java
+++ Assignment_1/TempCalculator.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class TempCalculator {
 
-
     public static void main(String[] args) {
 
     System.out.println("=================================================================");
@@ -11,6 +10,7 @@ public class TempCalculator {
     System.out.println();
     System.out.println("Please enter 5 temperatures in Celsius to get the average.");
 
+    //Gather user input //
     System.out.println();
     Scanner scanner = new Scanner(System.in);
 
@@ -37,8 +37,30 @@ public class TempCalculator {
     System.out.println();
     double tempAverage = (temp1 + temp2 + temp3 + temp4 + temp5) / 5;
 
-    System.out.println("The average temperature from the 5 temperatures is: " + tempAverage + " degrees Celsius.");
+    // Create an array to store the temperatures //
 
+    double[] tempArray = new double[5];
+
+    // Store the temperatures in the array //
+    tempArray[0] = temp1;
+    tempArray[1] = temp2;
+    tempArray[2] = temp3;
+    tempArray[3] = temp4;
+    tempArray[4] = temp5;
+
+    // Loops through to check if the number of temperatures in the array is above the average //
+        int greaterThanAverage = 0;
+
+        for (int i = 0; i < tempArray.length; i++) {
+            if (tempArray[i] > tempAverage) {
+                greaterThanAverage++;
+            }
+        }
+
+    System.out.println("The average temperature from the 5 temperatures is: " + tempAverage + " degrees Celsius.");
+    System.out.println();
+    System.out.println("The number of temperatures above the average is: " + greaterThanAverage);
+    System.out.println();
     scanner.close();
 
     }
