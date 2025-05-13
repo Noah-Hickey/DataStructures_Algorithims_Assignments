3cb27e017f9abe63f91a31fb86b46e6ba12db8c1
Noah-Hickey <Noah.Hickey@keyin.com>
2025-05-13 17:04:11 -0230

    Started project and created the files, began some print statements.

---
+++ Assignment_2/TheatreSeating.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TheatreSeating {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Theatre! Thank you for your ticket purchase!");
+
+        System.out.println("Please enter what seat you would like to choose!");
+
+    }
+}
