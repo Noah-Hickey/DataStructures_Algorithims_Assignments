1a737f2f128aa883e8a55a447f50a1f101410753
Noah-Hickey <Noah.Hickey@keyin.com>
2025-06-19 19:58:28 -0230

    Added the Node class.

--- Assignment4/src/main/java/UndoRedo.java
+++ Assignment4/src/main/java/UndoRedo.java
@@ -1,2 +1,21 @@
-public class UndoRedo {
+
+
+public class UndoRedo<T> {
+
+    private class Node {
+        T data; //Allows us to store any type of data type //
+        Node prev, next;
+
+        Node(T data) {
+            this.data = data;
+        }
+
+    }
+
+    private Node head; // Points to the start of the list //
+    private Node current; // Points to the current position in the list //
+
+
 }
+
+
