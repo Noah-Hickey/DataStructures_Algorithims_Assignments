9a93e41e163fce7fdd248c1c0f4543a84268833c
Noah-Hickey <Noah.Hickey@keyin.com>
2025-06-20 20:36:08 -0230

    Added in new methods for addState sand undo.

--- Assignment4/src/main/java/UndoRedo.java
+++ Assignment4/src/main/java/UndoRedo.java
@@ -15,7 +15,33 @@ public class UndoRedo<T> {
     private Node head; // Points to the start of the list //
     private Node current; // Points to the current position in the list //
 
+    public void addState(T state){
+        Node newNode = new Node(state);
+
+        if (head == null) {
+            head = newNode;
+            current = head;
+        } else{
+            if (current != null) current.next = null;
+
+            newNode.prev = current;
+            if (current != null) current.next = newNode;
+            current = newNode;
+        }
+    }
+
+    public T undo() {
+        if (current != null && current.next != null) {
+            current = current.next; // Move to the next state
+            return current.data; // Return the data of the new current state
+        }
+        return null;
+    }
 
 }
+
+
+
+
 
 
